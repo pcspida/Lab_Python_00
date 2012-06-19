25e455e2b6e7750b1a485bff52c60c36bff5b76b
pcspida <pcobby@gmail.com>
2012-06-19 16:08:47 +0000

    Source Code

---
+++ Day1.py
@@ -0,0 +1,20 @@
+print "        LAB WORK DAY 1 19/06/12"
+print "========================================"
+print " Kwabena Ansah"
+print "Hello World!!!"
+print "11/2 = ", 11/2
+print "11.0/2 =", 11.0/2
+print "3^27 = ",pow(3,27)
+print "(134533 + 6889980 - 84849)^3 = ", pow((134533 + 6889980 - 84849), 3)
+distLab2MarketInKilometer = 7.9
+walkingTime = 45
+distLab2MarketInMeters = distLab2MarketInKilometer*1000
+averageTimePerMeter = distLab2MarketInMeters / walkingTime
+print "The average time per meter is ", averageTimePerMeter
+
+kilo = 0.62137
+north = 4.7*kilo
+east = 9.4*kilo
+shortestDistance = pow(pow(north,2)+pow(east,2),0.5)
+print "The shortest distance is ", shortestDistance
+
